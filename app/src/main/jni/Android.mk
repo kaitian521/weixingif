31ffce6c6c741fa386a2057bceb2a7baf87e29ac
shaokai.li <shaokai.li@dian.fm>
2015-06-01 15:22:26 +0800

    remove useless android.mk

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -6,23 +6,8 @@
 # tree. An additional intellectual property rights grant can be found
 # in the file PATENTS.  All contributing project authors may
 # be found in the AUTHORS file in the root of the source tree.
-#include $(BUILD_SHARED_LIBRARY)
-#LOCAL_STATIC_LIBRARIES := cpufeatures
-#include $(call all-subdir-makefiles)
 LOCAL_ROOT_PATH := $(call my-dir)
-#include $(LOCAL_ROOT_PATH)/../../protobufStandalone/jni/zlib/Android.mk
-#include $(LOCAL_ROOT_PATH)/../../protobufStandalone/jni/google/protobuf/Android.mk
-#include $(LOCAL_ROOT_PATH)/zlib.mk
-#include $(LOCAL_ROOT_PATH)/protobuf.mk
-#include $(LOCAL_ROOT_PATH)/cpp-core-lib/Android.mk
-#include $(LOCAL_ROOT_PATH)/protobuf-standalone/Android.mk
-#include $(LOCAL_ROOT_PATH)/cpp-hongdian-session/Android.mk
-#include $(LOCAL_ROOT_PATH)/hdagent/Android.mk
-#include $(LOCAL_ROOT_PATH)/jni.mk
 
 include $(LOCAL_ROOT_PATH)/jni.mk
-#include $(LOCAL_ROOT_PATH)/MediaLibFFmpeg.mk
 
-#include $(LOCAL_ROOT_PATH)/HDClientNetworkFramework/Android.mk
-#include $(LOCAL_ROOT_PATH)/HDAgent.mk
 $(call import-module,android/cpufeatures)
