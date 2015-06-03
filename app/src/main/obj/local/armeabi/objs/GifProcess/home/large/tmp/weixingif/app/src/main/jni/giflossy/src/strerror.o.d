ebbaaa2947fda262b6ab2d4376f044da58dfc959
shaokai.li <shaokai.li@dian.fm>
2015-06-03 22:45:38 +0800

    remove

--- app/src/main/obj/local/armeabi/objs/GifProcess/home/large/tmp/weixingif/app/src/main/jni/giflossy/src/strerror.o.d
+++
@@ -1,2 +0,0 @@
-/home/large/tmp/weixingif/app/src/main/obj/local/armeabi/objs/GifProcess//home/large/tmp/weixingif/app/src/main/jni/giflossy/src/strerror.o: \
- /home/large/tmp/weixingif/app/src/main/jni/giflossy/src/strerror.c
